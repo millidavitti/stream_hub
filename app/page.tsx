ffb0e8bcfeb383a3de2e8aed79261c90f1e81a4b
davitti <abuadonald@gmail.com>
2024-01-18 00:03:48 +0200

    chore: database setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 import { UserButton } from "@clerk/nextjs";
+import mongoose from "mongoose";
 
 export default function Home() {
 	return <UserButton afterSignOutUrl='/' />;
