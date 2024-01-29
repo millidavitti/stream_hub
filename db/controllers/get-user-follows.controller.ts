2b9678ebd24aa36e351c0dc8e83eddaadace4daa
davitti <abuadonald@gmail.com>
2024-01-29 19:32:15 +0100

    task: excluded blocked follows from follows list

--- db/controllers/get-user-follows.controller.ts
+++ db/controllers/get-user-follows.controller.ts
@@ -3,6 +3,8 @@ import { connectdb } from "../connect";
 import { getAuthenticatedUser } from "../helpers/getAuthenticatedUser";
 import userModel from "../models/user.model";
 import "../models/follow.model";
+import { Block } from "../models/block.model";
+import { revalidatePath } from "next/cache";
 
 export default async function getFollows() {
 	const authenticatedUser = await getAuthenticatedUser();
@@ -10,18 +12,36 @@ export default async function getFollows() {
 	await connectdb("Get user follows!");
 	try {
 		if (authenticatedUser) {
+			const user = await userModel
+				.findOne({ authId: authenticatedUser.id })
+				.populate({
+					path: "blocks",
+				});
+
+			const blockedUsernames = user.blocks.map(
+				(block: Block) => block.blockedUsername,
+			);
+			console.log(blockedUsernames);
+
 			const follows = (await userModel
-				.findOne({ authId: authenticatedUser.id }, { follows: 1, userName: 1 })
+				.findOne(
+					{
+						authId: authenticatedUser.id,
+					},
+					{ follows: 1, userName: 1 },
+				)
 				.populate({
 					path: "follows",
+					match: { followUsername: { $nin: blockedUsernames } },
 					populate: {
 						path: "follow",
 						model: "User",
 					},
-				})
-				.orFail()) as Document | any;
+				})) as Document;
 
-			return follows.toObject();
+			revalidatePath("/", blockedUsernames);
+			console.log("Line 41: ", follows);
+			return follows?.toObject();
 		} else console.log("User not signed in!");
 	} catch (error) {
 		console.log(error);
