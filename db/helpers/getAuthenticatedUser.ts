375bbd9e127bff46828914c994fe5633ed1910a1
davitti <abuadonald@gmail.com>
2024-01-29 09:24:25 +0100

    mod: removed retry functionality

--- db/helpers/getAuthenticatedUser.ts
+++ db/helpers/getAuthenticatedUser.ts
@@ -1,14 +1,9 @@
 import { currentUser } from "@clerk/nextjs/server";
 
-export async function getAuthenticatedUser(retries = 3) {
-	if (retries < 1) {
-		console.log("Failed to get authenticated user after multiple (4) retries");
-		return null;
-	}
-
+export async function getAuthenticatedUser() {
 	try {
 		return await currentUser();
-	} catch {
-		return await getAuthenticatedUser(retries - 1);
+	} catch (error) {
+		console.log(error);
 	}
 }
