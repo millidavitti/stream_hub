29517c7ddec9a2238798bb39d0d5dd9fc2793b30
davitti <abuadonald@gmail.com>
2024-01-31 03:33:23 +0100

    mod: renamed file for consistency

--- app/(browse)/_components/navbar/actions.tsx
+++ app/(browse)/_components/navbar/actions.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { getAuthenticatedUser } from "@/db/helpers/getAuthenticatedUser";
+import { getAuthenticatedUser } from "@/db/helpers/get-authenticated-user";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { Clapperboard } from "lucide-react";
 import Link from "next/link";
