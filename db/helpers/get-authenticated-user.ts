32b7918f5388585a72c0b1e67cc54ce077e31e0b
davitti <abuadonald@gmail.com>
2024-01-31 05:02:02 +0100

    deleted file

--- db/helpers/get-authenticated-user.ts
+++
@@ -1,9 +0,0 @@
-import { currentUser } from "@clerk/nextjs";
-
-export async function getAuthenticatedUser() {
-	try {
-		return await currentUser();
-	} catch (error) {
-		console.log(error);
-	}
-}
